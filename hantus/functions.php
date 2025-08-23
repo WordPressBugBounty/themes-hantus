621f2c8720f92a86357378195ae2b98cf95be924
Sean Murphy <sean@iamseanmurphy.com>
2025-08-23 11:16:57 +0000

    17.5

--- hantus/functions.php
+++ hantus/functions.php
@@ -5,7 +5,7 @@ function hantus_setup() {
 	/**
 	 * Define Theme Version
 	 */
-	define( 'HANTUS_THEME_VERSION', '17.4' );
+	define( 'HANTUS_THEME_VERSION', '17.5' );
 	
 	/*
 	 * Make theme available for translation.
